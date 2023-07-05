d598068c02419cd031297a73cce0ce16b7ea0514
Nicolas <nicolasgfe3@gmail.com>
2023-07-04 22:48:03 -0300

    feat add swagger

--- src/main/java/com/juliano/task/controller/TaskController.java
+++ src/main/java/com/juliano/task/controller/TaskController.java
@@ -1,8 +1,10 @@
 package com.juliano.task.controller;
 
 import com.juliano.task.model.Task;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import jdk.jfr.Description;
 import org.hibernate.query.ReturnableType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -24,12 +26,15 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/task")
+@Description("Tasskkkk")
 public class TaskController {
 	@Autowired
+
 	private TaskService taskService;
 
 	@PostMapping
 	@Transactional
+	@Operation(description = "Created new task")
 	public ResponseEntity<TaskDTO> createTask(@RequestBody @Valid TaskForm taskForm) {
 		Task created = taskService.createTask(taskForm);
 		return ResponseEntity.status(HttpStatus.CREATED).body(new TaskDTO(created));
@@ -37,6 +42,7 @@ public class TaskController {
 
 	@PutMapping("/{id}")
 	@Transactional
+	@Operation(description = "Updated Task")
 	public ResponseEntity<TaskDTO> updateTask(@PathVariable Long id, @RequestBody @Valid UpdateTaskForm task) {
 		Optional<Task> optional = taskService.findById(id);
 		if (optional.isPresent()) {
@@ -48,6 +54,7 @@ public class TaskController {
 
 	@PatchMapping("/{id}")
 	@Transactional
+	@Operation(description = "Updated status task")
 	public ResponseEntity<TaskDTO> updateStatusTask(@PathVariable Long id, @RequestBody @Valid UpdateStatusTaskForm task) {
 		Optional<Task> optional = taskService.findById(id);
 		if (optional.isPresent()){
@@ -58,12 +65,14 @@ public class TaskController {
 	}
 
 	@GetMapping
+	@Operation(description = "Find Tasks")
 	public ResponseEntity<Page<TaskDTO>> getAllTasks(@PageableDefault(sort = "id", direction = Direction.ASC, page = 0, size = 10) Pageable pagination) {
 		return ResponseEntity.ok(taskService.getAllTasks(pagination));
 	}
 
 	@DeleteMapping("/{id}")
 	@Transactional
+	@Operation(description = "Delete Task")
 	public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
 		Optional<Task> optional = taskService.findById(id);
 		if (optional.isPresent()) {
